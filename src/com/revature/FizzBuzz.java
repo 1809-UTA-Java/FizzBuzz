a95eaa7da7d75ba43a65e49a439acc595e45ef4b
stevensly <steven.f.sly@protonmail.com>
2018-09-25 17:34:23 -0500

    First commit - Steven Sly

--- src/com/revature/FizzBuzz.java
+++ src/com/revature/FizzBuzz.java
@@ -6,10 +6,40 @@ package com.revature;
 * This program will run FizzBuzz. You can run this following the
 * directions in the README.
 *
-* @author YourNameHere
+* @author Steven Sly
 */
 class FizzBuzz {
 	public static void main (String[] args) {
-		//TODO: FizzBuzz
+		FizzBuzz(3, 5, 0, 100);
+	}
+	
+	/**
+	* This method takes integer paramters for the fizzNumber, the buzzNumber, the lowerBound, and the upperBound and 
+	* outputs the FizzBuzz between the lower and upper bound.
+	*/
+	public static void FizzBuzz(int fizzNumber, int buzzNumber, int lowerBound, int upperBound) {
+		// Loop to print out fizz buzz given method parameters
+		for (int i  = lowerBound; i < upperBound; i++) {
+			
+			// work around the case where i == 0 for modulus operation.
+			if (i != 0) {
+				// FizzBuzz
+				if ( (i % fizzNumber == 0) && (i % buzzNumber == 0) ) {
+					System.out.println("FizzBuzz");
+				// Fizz
+				} else if (i % fizzNumber == 0) {
+					System.out.println("Fizz");
+				// Buzz
+				} else if (i % buzzNumber == 0) {
+					System.out.println("Buzz");
+				// #
+				} else {
+					System.out.println(i);
+				}
+			// 0	
+			} else if (i == 0){
+				System.out.println(0);
+			}
+		} 
 	}
 }
