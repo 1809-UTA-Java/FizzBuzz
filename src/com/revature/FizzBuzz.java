d8aff7a6f16396830ad6e6306b230e1c276fa700
tim-deckert <tadeckert7@gmail.com>
2018-09-25 10:55:52 -0500

    My FizzBuzz attempt

--- src/com/revature/FizzBuzz.java
+++ src/com/revature/FizzBuzz.java
@@ -11,5 +11,23 @@ package com.revature;
 class FizzBuzz {
 	public static void main (String[] args) {
 		//TODO: FizzBuzz
+		if (args.length != 1) {
+			System.out.println("USAGE: java FizzBuzz [Number to go to inclusive]");
+			return;
+		}
+
+		for (int i = 0; i <= Integer.parseInt(args[0]); ++i) {
+			if (i % 3 == 0) {
+				System.out.printf("Fizz");
+			}
+			if (i % 5 == 0) {
+				System.out.printf("Buzz");
+			}
+			if (i % 3 != 0 && i % 5 != 0) {
+				System.out.printf("%d", i);
+			}
+
+			System.out.printf("\n");
+		}
 	}
 }
