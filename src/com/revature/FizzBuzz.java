3d888edd8d578058c0c608cb32a2bf6de61aa099
Developer <davidpaulhelmick@gmail.com>
2018-09-25 10:52:02 -0500

    Add the FizzBuzz code

--- src/com/revature/FizzBuzz.java
+++ src/com/revature/FizzBuzz.java
@@ -6,10 +6,19 @@ package com.revature;
 * This program will run FizzBuzz. You can run this following the
 * directions in the README.
 *
-* @author YourNameHere
+* @author David Helmick
 */
 class FizzBuzz {
 	public static void main (String[] args) {
-		//TODO: FizzBuzz
+		for (int i = 1; i <= 100; i++){
+			if (i % 3 == 0 && i % 5 == 0)
+				System.out.println("FizzBuzz");
+			else if (i % 3 == 0)
+				System.out.println("Fizz");
+			else if (i % 5 == 0)
+				System.out.println("Buzz");
+			else
+				System.out.println(i);
+		}
 	}
 }
