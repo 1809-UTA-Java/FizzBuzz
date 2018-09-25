0e514e8399604e2b5a3bf19d82e0be1ebf09e0d9
Morales-BA <moralesdafro@gmail.com>
2018-09-25 11:14:35 -0500

    FizzBuzz with no input

--- src/com/revature/FizzBuzz.java
+++ src/com/revature/FizzBuzz.java
@@ -11,5 +11,28 @@ package com.revature;
 class FizzBuzz {
 	public static void main (String[] args) {
 		//TODO: FizzBuzz
+
+		int iCap = 100;
+		String sOut = "1";
+
+		//if(args[0] != null)
+		//	iCap = Integer.parseInt(args[0]);
+
+		for(int iCntr = 1; iCntr <= iCap; iCntr++) {
+			if(iCntr % 3 == 0)
+				sOut += "Fizz";
+
+			if(iCntr % 5 == 0)
+				sOut += "Buzz";
+
+			if(sOut == "\0")
+				sOut = Integer.toString(iCntr);
+
+			System.out.print(sOut +", ");
+
+			sOut = "\0";
+		}
+
+		System.out.print("\n");
 	}
 }
