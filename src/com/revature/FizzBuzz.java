b87079c337d539a98c6c8932ad3a70abff57c717
Glenn Cariga <glenncariga@gmail.com>
2018-09-25 11:04:58 -0500

    filled out FizzBuzz

--- src/com/revature/FizzBuzz.java
+++ src/com/revature/FizzBuzz.java
@@ -11,5 +11,23 @@ package com.revature;
 class FizzBuzz {
 	public static void main (String[] args) {
 		//TODO: FizzBuzz
+		String fizzBuzz="";
+		for(int i = 1; i < 16 ; i++){
+			if(i %5==0){
+				fizzBuzz = "Buzz";
+			}
+			if(i%3 ==0){
+				fizzBuzz = "Fizz";
+			}
+			if(i %3 ==0 && i%5 ==0){
+				fizzBuzz = "FizzBuzz";
+			}
+			if(fizzBuzz==""){
+				System.out.println(i);
+			}else{
+				System.out.println(fizzBuzz);
+			}
+			fizzBuzz = "";
+		}
 	}
 }
