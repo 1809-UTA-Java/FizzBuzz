febe3d6f576d6402015d5887f939fc28c37f6d9c
Zach Leonard <zachariah.s.leonard@gmail.com>
2018-09-25 11:26:14 -0500

    Revised order of else if to properly catch multiples of 3&&5

--- src/com/revature/FizzBuzz.java
+++ src/com/revature/FizzBuzz.java
@@ -8,14 +8,14 @@ package com.revature;
 class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 1; i < 101; i++) {
-	    if (i % 3 == 0) {
-                System.out.print( "Fizz\n" );
-            }
+	    if ((i % 5 == 0) && (i % 3 == 0)) {
+                System.out.print( "FizzBuzz\n" );
+            }	    
             else if (i % 5 == 0) {
                 System.out.print( "Buzz\n" );
             }
-            else if ((i % 5 == 0) && (i % 3 == 0)) {
-                System.out.print( "Buzz\n" );
+            else if (i % 3 == 0) {
+                System.out.print( "Fizz\n" );
             }
             else {
                 System.out.print( i + "\n");
