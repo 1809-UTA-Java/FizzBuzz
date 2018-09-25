36eb7074ab5e685828b7be449a4b22ebdafe1cce
ddavaloo24 <daria.davaloO@gmail.com>
2018-09-25 10:46:55 -0500

    Second commit

--- src/com/revature/FizzBuzz.java
+++ src/com/revature/FizzBuzz.java
@@ -10,18 +10,18 @@ package com.revature;
 */
 class FizzBuzz {
 	public static void main (String[] args) {
-		for( int i = 0; i < args.length; i++ ) {
+		for( int i = 1; i < 101; i++ ) {
 			if( args[i] % 3 == 0 && args[i] % 5 == 0 ) {
-				System.out.println( "FizzBuzz" );			
+				System.out.println( "FizzBuzz, " );			
 			}
 			else if( args[i] % 3 == 0 ) {
-				System.out.println( "Fizz" );			
+				System.out.println( "Fizz, " );			
 			}
 			else if( args[i] % 5 == 0 ) {
-				System.out.println( "Buzz" );			
+				System.out.println( "Buzz, " );			
 			}
 			else{
-				System.out.println( args[i] );			
+				System.out.println( i + ", " );			
 			}
 
 		}
