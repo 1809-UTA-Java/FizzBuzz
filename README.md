c1ca84df429a16f6bcd9461a942632e2a3beac3e
Zach Leonard <zachariah.s.leonard@gmail.com>
2018-09-25 11:08:34 -0500

    Added the functionality to FizzBuzz.java and provided scripts for compilation and execution

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
 # Directions
-First, clone the repo.
->git clone <repo url>
+To run the program you can either use the sh script
+>./buildRun
+This script will compile and run the program
 
-Pull changes with
->git pull
-
-Checkout to a new branch:
->git checkout -b your-branch-name
-
-Make your changes to code
-When done:
->git push origin your-branch name
+In the event of insufficient file permissions, you will need to add
+execution permission to the sh script
+>chmod +x buildRun
--- src/com/revature/FizzBuzz.java
+++ src/com/revature/FizzBuzz.java
@@ -1,15 +1,22 @@
 package com.revature;
-
+//Javadoc style comment
 /**
-* This is a basic FizzBuzz attempt
-*
-* This program will run FizzBuzz. You can run this following the
-* directions in the README.
-*
-* @author Zach Leonarde
+* This is a basic FizzBuzz attempt.
+* This program will run FizzBuzz. You can run this following the directions in the README.
+* @author Zach Leonard
 */
 class FizzBuzz {
-	public static void main (String[] args) {
-		System.out.print("I am the almighty FizzBuzz!");
-	}
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+	    if (i % 3 == 0) {
+                System.out.print( "Fizz\n" );
+            }
+            else if (i % 5 == 0) {
+                System.out.print( "Buzz\n" );
+            }
+            else {
+                System.out.print( i + "\n");
+	    }
+        }
+    }
 }
