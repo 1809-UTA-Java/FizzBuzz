a1e541426e493715079aeb2abdb797036adf72bc
ddavaloo24 <daria.davaloO@gmail.com>
2018-09-25 10:53:39 -0500

    removed arg errors

--- src/com/revature/FizzBuzz.java
+++ src/com/revature/FizzBuzz.java
@@ -11,13 +11,13 @@ package com.revature;
 class FizzBuzz {
 	public static void main (String[] args) {
 		for( int i = 1; i < 101; i++ ) {
-			if( args[i] % 3 == 0 && args[i] % 5 == 0 ) {
+			if( i % 3 == 0 && i % 5 == 0 ) {
 				System.out.println( "FizzBuzz, " );			
 			}
-			else if( args[i] % 3 == 0 ) {
+			else if( i % 3 == 0 ) {
 				System.out.println( "Fizz, " );			
 			}
-			else if( args[i] % 5 == 0 ) {
+			else if( i % 5 == 0 ) {
 				System.out.println( "Buzz, " );			
 			}
 			else{
