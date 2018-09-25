5b743478d42f7a7d8a7f7997cdecaee5903c46e8
ddavaloo24 <daria.davaloO@gmail.com>
2018-09-25 10:42:51 -0500

    First FizzBuzz commit

--- src/com/revature/FizzBuzz.java
+++ src/com/revature/FizzBuzz.java
@@ -6,10 +6,24 @@ package com.revature;
 * This program will run FizzBuzz. You can run this following the
 * directions in the README.
 *
-* @author YourNameHere
+* @author Daria Davaloo
 */
 class FizzBuzz {
 	public static void main (String[] args) {
-		//TODO: FizzBuzz
+		for( int i = 0; i < args.length; i++ ) {
+			if( args[i] % 3 == 0 && args[i] % 5 == 0 ) {
+				System.out.println( "FizzBuzz" );			
+			}
+			else if( args[i] % 3 == 0 ) {
+				System.out.println( "Fizz" );			
+			}
+			else if( args[i] % 5 == 0 ) {
+				System.out.println( "Buzz" );			
+			}
+			else{
+				System.out.println( args[i] );			
+			}
+
+		}
 	}
 }
