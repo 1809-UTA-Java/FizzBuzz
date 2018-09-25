765b0f1d61d9cc63fe323ccc9d0bebef935c85d7
Leonardo DeLeon <lqdeleon@gmail.com>
2018-09-25 10:53:53 -0500

    initial branch commit

--- src/com/revature/FizzBuzz.java
+++ src/com/revature/FizzBuzz.java
@@ -6,10 +6,11 @@ package com.revature;
 * This program will run FizzBuzz. You can run this following the
 * directions in the README.
 *
-* @author YourNameHere
+* @author Leonardo De Leon
 */
 class FizzBuzz {
 	public static void main (String[] args) {
 		//TODO: FizzBuzz
+		System.out.println("Hey there fellow FizzBuzzler, this is Leonardo");
 	}
 }
