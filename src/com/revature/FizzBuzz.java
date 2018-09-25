af05e08887fc67f6c28a5c1417d2aeeddb1f21f7
selsuean <selsuean@gmail.com>
2018-09-25 18:03:02 -0500

    first fizzbuzz attempt

--- src/com/revature/FizzBuzz.java
+++ src/com/revature/FizzBuzz.java
@@ -6,19 +6,30 @@ package com.revature;
 * This program will run FizzBuzz. You can run this following the
 * directions in the README.
 *
-* @author David Helmick
+* @author Su Ean Lim
 */
 class FizzBuzz {
 	public static void main (String[] args) {
-		for (int i = 1; i <= 100; i++){
-			if (i % 3 == 0 && i % 5 == 0)
-				System.out.println("FizzBuzz");
-			else if (i % 3 == 0)
-				System.out.println("Fizz");
-			else if (i % 5 == 0)
-				System.out.println("Buzz");
-			else
-				System.out.println(i);
+		
+		int firstNum;
+		int secondNum;
+		
+		for (int i = 1; i <= 100; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("FizzBuzz\n");
+			}
+			if (i % 3 == 0) {
+				System.out.println("Fizz\n");	
+			}
+			if (i % 5 == 0) {
+				System.out.println("Buzz\n");	
+			}
+			else {
+				System.out.print("%d\n", i);
+			}
 		}
+		
+		
+
 	}
 }
